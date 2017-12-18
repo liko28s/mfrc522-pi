b91339a261a1a4313fd4841afd80a4601b6f5908
Ramiro Alvarez <liko28s@gmail.com>
2017-12-18 16:12:11 -0500

    - Dumping script in Node.js, based on [MFRC522-node](https://github.com/liko28s/MFRC522-node), to work with nw.js or any node.js project

---
+++ read.js
@@ -0,0 +1,45 @@
+var MFRC522 = function(){};
+MFRC522.prototype.spawn = require('child_process').spawn;
+MFRC522.prototype.mfrc522py = null
+MFRC522.prototype.callback = null
+MFRC522.prototype.dataRegex = new RegExp('"UID": [[0-9,]{4}');
+
+MFRC522.prototype.start = function (callback) {
+    this.callback = callback;
+
+    this.mfrc522py = this.spawn('python3', ['-u', __dirname+'/dump.py']);
+
+    var _this = this;
+    this.mfrc522py.stdout.on('data', function (data) {
+        var card = data.toString('utf8');
+        end_json = _this.dataRegex.exec(card);
+        if(end_json =! null) {
+            _this.callback.onData(card)
+        }
+    });
+
+    this.mfrc522py.on('exit', function () {
+        _this.callback.onExit();
+    });
+
+    _this.callback.onStart();
+};
+
+mfrc522 = new MFRC522();
+
+var Callback = function () {
+    this.onStart = function () {
+        console.log('Waiting Cards');
+    };
+
+    this.onData = function (data) {
+        var dataJson = JSON.parse(data);
+        console.log('Read Card');
+    };
+
+    this.onExit = function () {
+        console.log('Exit');
+    }
+};
+
+mfrc522.start(new Callback());
